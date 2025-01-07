0d6dee144e599278871582a769b8a407cd4fb68d
Daniel Hafner <dani@danionnet.ch>
2025-01-07 13:33:39 +0100

    added tpnetwork stuff

---
+++ Tests.TpNetwork/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.TpNetwork
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(HttpRequestAsync().Result);
+        }
+
+        private static async Task<string> HttpRequestAsync()
+        {
+            string result = string.Empty;
+
+            using (var tcp = new TcpClient("127.0.0.1", 9901))
+            using (var stream = tcp.GetStream())
+            {
+                tcp.SendTimeout = 500;
+                tcp.ReceiveTimeout = 1000;
+                // Send request headers
+                var builder = new StringBuilder();
+                builder.AppendLine("hello world");
+                var header = Encoding.ASCII.GetBytes(builder.ToString());
+                var gzipRequest = new GZipStream(stream, CompressionMode.Compress);
+                await gzipRequest.WriteAsync(header, 0, header.Length);
+
+                // Send payload data if you are POST request
+                //await stream.WriteAsync(data, 0, data.Length);
+
+                // receive data
+                using (var memory = new MemoryStream())
+                {
+                    await stream.CopyToAsync(memory);
+                    memory.Position = 0;
+                    var data = memory.ToArray();
+
+                    var index = BinaryMatch(data, Encoding.ASCII.GetBytes("\r\n\r\n")) + 4;
+                    var headers = Encoding.ASCII.GetString(data, 0, index);
+                    memory.Position = index;
+
+                    if (headers.IndexOf("Content-Encoding: gzip") > 0)
+                    {
+                        using (GZipStream decompressionStream = new GZipStream(memory, CompressionMode.Decompress))
+                        using (var decompressedMemory = new MemoryStream())
+                        {
+                            decompressionStream.CopyTo(decompressedMemory);
+                            decompressedMemory.Position = 0;
+                            result = Encoding.UTF8.GetString(decompressedMemory.ToArray());
+                        }
+                    }
+                    else
+                    {
+                        result = Encoding.UTF8.GetString(data, index, data.Length - index);
+                        //result = Encoding.GetEncoding("gbk").GetString(data, index, data.Length - index);
+                    }
+                }
+
+                //Debug.WriteLine(result);
+                return result;
+            }
+        }
+
+        private static int BinaryMatch(byte[] input, byte[] pattern)
+        {
+            int sLen = input.Length - pattern.Length + 1;
+            for (int i = 0; i < sLen; ++i)
+            {
+                bool match = true;
+                for (int j = 0; j < pattern.Length; ++j)
+                {
+                    if (input[i + j] != pattern[j])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
